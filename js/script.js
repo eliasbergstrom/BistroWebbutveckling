0f4eeb9e51cb14bef00018f765a96a747addc4a3
eliasbergstrom <elias.bergstrom@student.tabyensklida.se>
2021-02-23 12:26:09 +0100

    .

---
+++ js/script.js
@@ -0,0 +1,11 @@
+  function changeImage() {
+
+        if (document.getElementById("bildspel").src == "img/mat.jpg")
+        {
+            document.getElementById("bildspel").src = "img/restaurang.jpg";
+        }
+        else
+        {
+            document.getElementById("bildspel").src = "img/mat.jpg";
+        }
+    }
